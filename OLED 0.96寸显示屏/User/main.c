3a910efdd6bae6cb7de0873f7c7131116da52b47
liuliming <1104257233@qq.com>
2019-12-26 18:50:25 +0800

    rename

---
+++ OLED 0.96寸显示屏/User/main.c
@@ -0,0 +1,55 @@
+#include "headfile.h"
+
+uint8 Pit_flag = 0;
+
+void MRT0_DriverIRQHandler(void)
+{
+	if(MRT_FLAG_READ(MRT_CH0))
+	{
+		MRT_FLAG_CLR(MRT_CH0);
+		Pit_flag = 1;
+	}
+}
+
+int main(void)
+{
+	int32 times1, times2, time;
+	DisableInterrupts;
+	get_clk();//获取时钟频率  务必保留
+	
+	OLED_Init();
+	mrt_pit_init_ms(MRT_CH0, 50);
+	set_irq_priority(MRT0_IRQn,0);
+	
+	//总中断最后开启
+	EnableInterrupts;
+	
+	while(1)
+	{
+		if(Pit_flag == 1)
+		{
+			Pit_flag = 0;
+			times1 = MRT0->CHANNEL[MRT_CH0].TIMER;
+			
+			OLED_P6x8Int(0, 0, 121, 3);
+			OLED_P6x8Int(0, 1, 12, 3);
+			OLED_P6x8Int(0, 2, 0, 3);
+			OLED_P6x8Int(0, 3, -121, 3);
+			OLED_P6x8Int(0, 4, 121, -3);
+			OLED_P6x8Flo(0, 5, -12222.22, -5);
+			OLED_P6x8Flo(0, 6, 122222.22, 6);
+			OLED_P6x8Int(64, 0, 121, 3);
+			OLED_P6x8Int(64, 1, 12, 3);
+			OLED_P6x8Int(64, 2, 1, 2);
+			OLED_P6x8Int(64, 3, -1, 1);
+			OLED_P6x8Int(64, 4, 0, 1);
+			OLED_P6x8Int(64, 5, -21, 2);
+			OLED_P6x8Int(64, 6, -123, 2);
+			times2 = MRT0->CHANNEL[MRT_CH0].TIMER;
+			time = times1 - times2;
+			OLED_P6x8Flo(0, 7, (float)time/(1000 * main_clk_mhz), 2);
+			OLED_P6x8Str(64, 7, "ms");
+		}
+	}
+}
+
